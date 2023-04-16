ddb4d34bea029ae2bfc7c854133229b8e4e98db4
Tanner <Tanner@DESKTOP-SBM2OA1>
2023-04-15 21:07:32 -0500

    Added Tracking and Updating of Multiple Conversations
    Added the needed functionality to save the existing conversation whenever a user starts a new conversation.
    
    And loads the last saved conversation whenever you visit the page again.

--- app.js
+++ app.js
@@ -96,8 +96,11 @@ function AppViewModel() {
     }
 
     function loadMessagesFromLocalStorage() {
-        const storedMessages = localStorage.getItem('gpt-messages');
-        return storedMessages ? JSON.parse(storedMessages) : '';
+        const storedMessages = localStorage.getItem('gpt-conversations');
+        let parsedConversations = storedMessages ? JSON.parse(storedMessages) : [];
+
+
+        return parsedConversations.length ? parsedConversations[parsedConversations.length - 1].messageHistory : []; 
     }
 
 
@@ -139,10 +142,76 @@ function AppViewModel() {
         return formattedMessage;
     };
 
-    self.clearMessages = function () {
+    self.clearMessages = async function () {
+        const newConversation = {
+            messageHistory: self.messages(),
+            title: await getConversationTitleFromGPT()
+        };
+    
+        if (!localStorage.getItem("gpt-conversations")) {
+            localStorage.setItem("gpt-conversations", JSON.stringify([newConversation]));
+        } else {
+            let storedConversations = JSON.parse(localStorage.getItem("gpt-conversations"));
+    
+            // Find the index of the conversation that has a 15% or more match in assistant's answers
+            const conversationIndex = storedConversations.findIndex((storedConversation) => {
+                const storedAssistantAnswers = storedConversation.messageHistory.filter(msg => msg.role === 'assistant');
+                const newAssistantAnswers = newConversation.messageHistory.filter(msg => msg.role === 'assistant');
+    
+                const matchingAnswers = storedAssistantAnswers.filter((storedAnswer, index) => {
+                    return index < newAssistantAnswers.length && storedAnswer.content === newAssistantAnswers[index].content;
+                });
+    
+                const matchingPercentage = (matchingAnswers.length / storedAssistantAnswers.length) * 100;
+    
+                return matchingPercentage >= 15;
+            });
+    
+            if (conversationIndex !== -1) {
+                // Update the existing conversation's messageHistory with the new values
+                storedConversations[conversationIndex].messageHistory = newConversation.messageHistory;
+            } else {
+                // If the conversation doesn't exist, add it to the stored conversations
+                storedConversations.push(newConversation);
+            }
+    
+            localStorage.setItem("gpt-conversations", JSON.stringify(storedConversations));
+        }
+    
         self.messages.removeAll();
         localStorage.removeItem("gpt-messages");
     };
+    
+
+    async function getConversationTitleFromGPT() {
+        try {
+            let tempMessages = self.messages().slice(0);
+            tempMessages.push({ role: 'user', content: "Summarize our conversation in 5 words or less." });
+            const response = await fetch("https://api.openai.com/v1/chat/completions", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${apiKey.value.trim() || 'Missing API Key'}`,
+                },
+                body: JSON.stringify({
+                    model: self.selectedModel(),
+                    messages: tempMessages,
+                    temperature: self.sliderValue() * 0.01
+                }),
+            });
+
+            const result = await response.json();
+
+            if (result.choices && result.choices.length > 0) {
+                return result.choices[0].message.content;
+            } else {
+                return "I'm sorry, I couldn't generate a response.";
+            }
+        } catch (error) {
+            console.error("Error fetching GPT response:", error);
+            return "An error occurred while fetching a response.";
+        }
+    }
 
 
     self.scrollToBottom = function () {
