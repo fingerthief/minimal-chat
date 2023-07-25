330a654d38b0b4c7771ed35608f53d68f3856382
Tanner <tmiddleton@middmail.net>
2023-07-25 18:01:28 -0500

    Add error message for failed PaLM response

--- js/palm-api-access.js
+++ js/palm-api-access.js
@@ -5,25 +5,30 @@ const MODEL_NAME = "chat-bison-001";
 export async function fetchPalmResponse(messages) {
     const API_KEY = localStorage.getItem("palmKey");
     const ATTITUDE = localStorage.getItem("palm-attitude");
-
-    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta2/models/${MODEL_NAME}:generateMessage?key=${API_KEY}`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                prompt: { messages : messages },
-                temperature: ATTITUDE * 0.01,
-                candidate_count: 1          
-            }),
-        });
-
-        const result = await response.json();
-        const reponseText = result.candidates[0].content;
-
-        console.log(result.candidates[0].content);
-
-        return reponseText;
+    try {
+        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta2/models/${MODEL_NAME}:generateMessage?key=${API_KEY}`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    prompt: { messages : messages },
+                    temperature: ATTITUDE * 0.01,
+                    candidate_count: 1          
+                }),
+            });
+    
+            const result = await response.json();
+            const reponseText = result.candidates[0].content;
+    
+            console.log(result.candidates[0].content);
+    
+            return reponseText;
+    }
+    catch (err) {
+        console.error("Error fetching PaLM Response:", err);
+        return "An error occurred while fetching PaLM response. Please try again.";
+    }
         
 }
 
@@ -47,29 +52,5 @@ export async function fetchPalmConversationTitle(messages) {
 
         console.log(result.candidates[0].content);
 
-        return reponseText;
-        
+        return reponseText;       
 }
-
-export async function savePalmMessages(currentMessages) {
-    const savedMessages = currentMessages().map(message => ({
-        content: message.content
-    }));
-
-    // Find the index of the selected conversation in storedConversations
-    // const conversationIndex = self.storedConversations().findIndex(conversation => conversation.title === self.selectedConversation().title);
-
-    // if (conversationIndex !== -1) {
-    //     // Update the message history of the selected conversation
-    //     self.storedConversations()[conversationIndex].messageHistory = savedMessages;
-    // } 
-    // else {
-    //     if (JSON.parse(self.selectedAutoSaveOption())) {
-    //         await this.saveNewConversations();
-    //     }
-    // }
-
-    // // Save the updated conversations to localStorage
-    // localStorage.setItem("gpt-conversations", JSON.stringify(self.storedConversations()));
-};
-
