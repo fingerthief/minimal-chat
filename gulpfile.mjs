2773c49c5d08911720b055885da7f0ff3e8a55a5
Tanner <tmiddleton@middmail.net>
2023-08-19 10:39:19 -0500

    gulpfile cleanup and improvements

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,8 +1,5 @@
 import gulp from 'gulp';
-import plugins from 'gulp-load-plugins';
 import newer from 'gulp-newer';
-import inject from 'gulp-inject-string';
-import fs from 'fs';
 import rev from 'gulp-rev';
 import revReplace from 'gulp-rev-replace';
 import rename from 'gulp-rename';
@@ -26,51 +23,10 @@ const node_modules = './node_modules/**/*';
 const webfonts = './webfonts/**';
 
 const dest = './public';
-
-// Define 'del' as a variable, and import it using a dynamic import
 let del;
 
 gulp.task('import-del', async () => {
   del = (await import('del')).deleteSync;
-});
-
-gulp.task('delete-index', async () => {
-  if (!del) {
-    await gulp.series('import-del')();
-  }
-
-  //delete ./public/index.html
-  await del('./public/index.html');
-});
-
-gulp.task('copy', () => {
-  return gulp
-    .src(srcFiles, { base: './' }) // Added the 'base' option to keep folder structure
-    .pipe(newer(dest))
-    .pipe(gulp.dest(dest));
-});
-
-gulp.task('add-gtag', function (done) {
-  const gtagCode = `
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-R42YRRG1Y1"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
-    gtag('js', new Date());
-
-    gtag('config', 'G-R42YRRG1Y1');
-</script>
-`;
-
-  // Add a delay before running the task
-  setTimeout(() => {
-    gulp
-      .src('./public/index.html')
-      .pipe(inject.after('</head>', gtagCode))
-      .pipe(gulp.dest('public'))
-      .on('end', done);
-  }, 3000);
 });
 
 gulp.task('copy', () => {
@@ -92,22 +48,25 @@ gulp.task('revReplace', () => {
 });
 
 gulp.task('copy_modules', function(){
-  return gulp.src(node_modules)  // This is the source folder which you want to copy
+  return gulp.src(node_modules)
+      .pipe(newer(dest + "/node_modules"))  // This is the source folder which you want to copy
       .pipe(gulp.dest(dest + "/node_modules"));
 });
 
 gulp.task('copy-fonts', function(){
   return gulp.src(webfonts)
+      .pipe(newer(dest + "/webfonts"))
       .pipe(gulp.dest(dest + "/webfonts")); // This is the destination folder where you want to paste
 });
 
 gulp.task('copy-images', function(){
-  return gulp.src("./images/**")
+  return gulp.src("./images")
+      .pipe(newer(dest + "/images"))
       .pipe(gulp.dest(dest + "/images")); // This is the destination folder where you want to paste
 });
 
 gulp.task('clean', async () => {
-  return await del([dest + '/**/*', '!' + dest], { force: true });
+  return await del([dest + '/**/*', '!' + dest, '!' + dest + '/webfonts/**', '!' + dest + '/node_modules/**'], { force: true });
 });
 
 gulp.task('rename-index', function() {
