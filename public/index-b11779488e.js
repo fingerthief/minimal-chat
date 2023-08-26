a9b493a635e249209ccd2fafea9b7f3a8bf1bc33
Tanner <tmiddleton@middmail.net>
2023-08-25 19:48:01 -0500

    Repo cleanup

--- public/index-b11779488e.js
+++
@@ -1,7 +0,0 @@
-import '../node_modules/markdown/lib/markdown.js'; 
-import '../node_modules/knockout/build/output/knockout-latest.js';
-import { AppViewModel } from './js/app-view-model-c4be410309.js';
-
-// Bind the ViewModel
-const ko = window.ko;
-ko.applyBindings(new AppViewModel());
