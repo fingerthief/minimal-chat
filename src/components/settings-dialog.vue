6f0279ff38ffe2de28923b7c6358022f319da404
Tanner <tmiddleton@middmail.net>
2024-05-04 00:03:46 -0500

    Update version number

--- src/components/settings-dialog.vue
+++ src/components/settings-dialog.vue
@@ -73,7 +73,7 @@ function toggleSidebar() {
             <span @click="reloadPage">
                 <RefreshCcw :size="23" :stroke-width="2" />
             </span>
-            Settings | V5.1.8
+            Settings | V5.1.9
         </h2>
     </div>
     <div class="sidebar-content-container">
