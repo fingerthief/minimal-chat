66a6952280073d5a4e64ebe34fb3582dca0cc8f2
Tanner <tmiddleton@middmail.net>
2024-06-23 16:27:46 -0500

    Expanded list of registered languages for syntax highlighting to cover all the major langauges

--- src/components/controls/MessageItem.vue
+++ src/components/controls/MessageItem.vue
@@ -34,18 +34,50 @@ import { updateUIWrapper } from '@/libs/utils/general-utils';
 import { saveMessagesHandler } from '@/libs/conversation-management/useConversations';
 import 'swiped-events';
 import hljs from 'highlight.js/lib/core';
-import javascript from 'highlight.js/lib/languages/javascript';
-import c from 'highlight.js/lib/languages/c';
-import csharp from 'highlight.js/lib/languages/csharp';
-import python from 'highlight.js/lib/languages/python';
+
 import 'highlight.js/styles/github-dark.css';
 import MarkdownIt from 'markdown-it';
 import Avatar from 'primevue/avatar';
 
+import c from 'highlight.js/lib/languages/c';
+import javascript from 'highlight.js/lib/languages/javascript';
+import python from 'highlight.js/lib/languages/python';
+import java from 'highlight.js/lib/languages/java';
+import cpp from 'highlight.js/lib/languages/cpp';
+import csharp from 'highlight.js/lib/languages/csharp';
+import php from 'highlight.js/lib/languages/php';
+import ruby from 'highlight.js/lib/languages/ruby';
+import swift from 'highlight.js/lib/languages/swift';
+import go from 'highlight.js/lib/languages/go';
+import rust from 'highlight.js/lib/languages/rust';
+import typescript from 'highlight.js/lib/languages/typescript';
+import kotlin from 'highlight.js/lib/languages/kotlin';
+import scala from 'highlight.js/lib/languages/scala';
+import html from 'highlight.js/lib/languages/xml';
+import css from 'highlight.js/lib/languages/css';
+import sql from 'highlight.js/lib/languages/sql';
+import bash from 'highlight.js/lib/languages/bash';
+import powershell from 'highlight.js/lib/languages/powershell';
+
 hljs.registerLanguage('javascript', javascript);
-hljs.registerLanguage('c', c);
-hljs.registerLanguage('csharp', csharp);
 hljs.registerLanguage('python', python);
+hljs.registerLanguage('java', java);
+hljs.registerLanguage('c', c);
+hljs.registerLanguage('cpp', cpp);
+hljs.registerLanguage('csharp', csharp);
+hljs.registerLanguage('php', php);
+hljs.registerLanguage('ruby', ruby);
+hljs.registerLanguage('swift', swift);
+hljs.registerLanguage('go', go);
+hljs.registerLanguage('rust', rust);
+hljs.registerLanguage('typescript', typescript);
+hljs.registerLanguage('kotlin', kotlin);
+hljs.registerLanguage('scala', scala);
+hljs.registerLanguage('html', html);
+hljs.registerLanguage('css', css);
+hljs.registerLanguage('sql', sql);
+hljs.registerLanguage('bash', bash);
+hljs.registerLanguage('powershell', powershell);
 
 // Props
 const props = defineProps({
